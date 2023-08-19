da65c6d17a3d04942ec5b9f32f5badd939e4df01
Gurgen-me <gurgenmelqonyan601@gmail.com>
2023-08-19 18:59:42 +0300

    fixed the footer

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ let navbar = document.querySelector('.navbar');
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
 }
+
+const INPUT = document.querySelector("input");
+
+const TOGGLE = () => {
+  document.documentElement.className = INPUT.checked ? "use-subgrid" : "";
+};
+
+const CHANGE = () => {
+  if (!document.startViewTransition) TOGGLE();
+  else document.startViewTransition(TOGGLE);
+};
+
+INPUT.addEventListener("change", CHANGE);
