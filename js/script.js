91f5537275dc83aefe9198d9bf435d1043a9045b
Gurgen-me <gurgenmelqonyan601@gmail.com>
2023-08-21 20:47:32 +0300

    new sorts

--- js/script.js
+++ js/script.js
@@ -1,24 +1,39 @@
+// бургер меню
 let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
 }
 
-const likeBtn = document.querySelector('.user-likes')
-likeBtn.addEventListener('click', countLikes)
-function countLikes() {
-    let count = 0
-    const likeToRed = document.querySelector('.vector')
-    const countPlace = document.getElementById('likesCounter')
-    if (likeToRed.classList.contains('_active') == true) {
-        likeToRed.classList.remove('_active');
-        count - 1;
-        countPlace.innerText = count
-    }
-    else {
-        count += 1;
-        likeToRed.classList.add('_active');
-        countPlace.innerText = count
-    }
-}   
 
+// Лайки
+const likeBtn = document.querySelectorAll('.user-likes')
+
+for(let i = 0 ; i <likeBtn.length; i++){
+    likeBtn[i].addEventListener('click', countLikes)
+    function countLikes() {
+        let count = 0
+        const likeToRed = document.querySelectorAll('.vector')
+        const countPlace = document.getElementsByClassName('likesCounter')
+        if (likeToRed[i].classList.contains('_active') == true) {
+            likeToRed[i].classList.remove('_active');
+            count - 1;
+            countPlace[i].innerText = count
+        }
+        else {
+            count += 1;
+            likeToRed[i].classList.add('_active');
+            countPlace[i].innerText = count
+        }
+    }  
+}
+
+// Сортировка
+
+const container =
+document.querySelector('.news-count');
+const sort = document.getElementById('sort');
+
+sort.addEventListener("click",()=>{
+    container.classList.toggle("news-count-r")
+});
